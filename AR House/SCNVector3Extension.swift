65ed4c28096ea7827c6706a089909dca283a1a94
Denis Bystruev <dbystruev@me.com>
2018-06-10 11:18:46 +0300

    Extended SCNVector3 with more functions

--- AR House/SCNVector3Extension.swift
+++ AR House/SCNVector3Extension.swift
@@ -12,7 +12,19 @@ extension SCNVector3 {
         return SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
     }
     
+    static func + (left: SCNVector3, right: Float) -> SCNVector3 {
+        return SCNVector3(left.x + right, left.y + right, left.z + right)
+    }
+    
+    static func + (left: Float, right: SCNVector3) -> SCNVector3 {
+        return SCNVector3(left + right.x, left + right.y, left + right.z)
+    }
+    
     static func += (left: inout SCNVector3, right: SCNVector3) {
+        left = left + right
+    }
+    
+    static func += (left: inout SCNVector3, right: Float) {
         left = left + right
     }
     
@@ -20,7 +32,15 @@ extension SCNVector3 {
         return SCNVector3(left.x - right.x, left.y - right.y, left.z - right.z)
     }
     
+    static func - (left: SCNVector3, right: Float) -> SCNVector3 {
+        return SCNVector3(left.x - right, left.y - right, left.z - right)
+    }
+    
     static func -= (left: inout SCNVector3, right: SCNVector3) {
+        left = left - right
+    }
+    
+    static func -= (left: inout SCNVector3, right: Float) {
         left = left - right
     }
     
@@ -32,7 +52,15 @@ extension SCNVector3 {
         return SCNVector3(left * right.x, left * right.y, left * right.z)
     }
     
+    static func *= (left: inout SCNVector3, right: Float) {
+        left = left * right
+    }
+    
     static func / (left: SCNVector3, right: Float) -> SCNVector3 {
         return SCNVector3(left.x / right, left.y / right, left.z / right)
+    }
+    
+    static func /= (left: inout SCNVector3, right: Float) {
+        left = left / right
     }
 }
