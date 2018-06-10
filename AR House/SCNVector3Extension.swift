0540c1b4804260a02a8d11a279bf27f457bd2c50
Denis Bystruev <dbystruev@me.com>
2018-06-10 14:10:00 +0300

    Made SCNVector3 extension generic

--- AR House/SCNVector3Extension.swift
+++ AR House/SCNVector3Extension.swift
@@ -1,8 +1,8 @@
 //
 //  SCNVector3Extension.swift
-//  AR House
 //
 //  Created by Denis Bystruev on 03/06/2018.
+//  Last edit by Denis Bystruev on 10/06/2018.
 //  Copyright © 2018 Denis Bystruev. All rights reserved.
 //
 import SceneKit
@@ -12,19 +12,32 @@ extension SCNVector3 {
         return SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
     }
     
-    static func + (left: SCNVector3, right: Float) -> SCNVector3 {
-        return SCNVector3(left.x + right, left.y + right, left.z + right)
+    static func +<T: BinaryFloatingPoint> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x + Float(right), left.y + Float(right), left.z + Float(right))
     }
     
-    static func + (left: Float, right: SCNVector3) -> SCNVector3 {
-        return SCNVector3(left + right.x, left + right.y, left + right.z)
+    static func +<T: BinaryInteger> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x + Float(right), left.y + Float(right), left.z + Float(right))
+    }
+    
+    
+    static func +<T: BinaryFloatingPoint> (left: T, right: SCNVector3) -> SCNVector3 {
+        return SCNVector3(Float(left) + right.x, Float(left) + right.y, Float(left) + right.z)
+    }
+    
+    static func +<T: BinaryInteger> (left: T, right: SCNVector3) -> SCNVector3 {
+        return SCNVector3(Float(left) + right.x, Float(left) + right.y, Float(left) + right.z)
     }
     
     static func += (left: inout SCNVector3, right: SCNVector3) {
         left = left + right
     }
     
-    static func += (left: inout SCNVector3, right: Float) {
+    static func +=<T: BinaryFloatingPoint> (left: inout SCNVector3, right: T) {
+        left = left + right
+    }
+    
+    static func +=<T: BinaryInteger> (left: inout SCNVector3, right: T) {
         left = left + right
     }
     
@@ -32,35 +45,63 @@ extension SCNVector3 {
         return SCNVector3(left.x - right.x, left.y - right.y, left.z - right.z)
     }
     
-    static func - (left: SCNVector3, right: Float) -> SCNVector3 {
-        return SCNVector3(left.x - right, left.y - right, left.z - right)
+    static func -<T: BinaryFloatingPoint> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x - Float(right), left.y - Float(right), left.z - Float(right))
+    }
+    
+    static func -<T: BinaryInteger> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x - Float(right), left.y - Float(right), left.z - Float(right))
     }
     
     static func -= (left: inout SCNVector3, right: SCNVector3) {
         left = left - right
     }
     
-    static func -= (left: inout SCNVector3, right: Float) {
+    static func -=<T: BinaryFloatingPoint> (left: inout SCNVector3, right: T) {
         left = left - right
     }
     
-    static func * (left: SCNVector3, right: Float) -> SCNVector3 {
-        return SCNVector3(left.x * right, left.y * right, left.z * right)
+    static func -=<T: BinaryInteger> (left: inout SCNVector3, right: T) {
+        left = left - right
     }
     
-    static func * (left: Float, right: SCNVector3) -> SCNVector3 {
-        return SCNVector3(left * right.x, left * right.y, left * right.z)
+    static func *<T: BinaryFloatingPoint> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x * Float(right), left.y * Float(right), left.z * Float(right))
     }
     
-    static func *= (left: inout SCNVector3, right: Float) {
+    static func *<T: BinaryInteger> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x * Float(right), left.y * Float(right), left.z * Float(right))
+    }
+    
+    static func *<T: BinaryFloatingPoint> (left: T, right: SCNVector3) -> SCNVector3 {
+        return SCNVector3(Float(left) * right.x, Float(left) * right.y, Float(left) * right.z)
+    }
+    
+    static func *<T: BinaryInteger> (left: T, right: SCNVector3) -> SCNVector3 {
+        return SCNVector3(Float(left) * right.x, Float(left) * right.y, Float(left) * right.z)
+    }
+    
+    static func *=<T: BinaryFloatingPoint> (left: inout SCNVector3, right: T) {
         left = left * right
     }
     
-    static func / (left: SCNVector3, right: Float) -> SCNVector3 {
-        return SCNVector3(left.x / right, left.y / right, left.z / right)
+    static func *=<T: BinaryInteger> (left: inout SCNVector3, right: T) {
+        left = left * right
     }
     
-    static func /= (left: inout SCNVector3, right: Float) {
+    static func /<T: BinaryFloatingPoint> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x / Float(right), left.y / Float(right), left.z / Float(right))
+    }
+    
+    static func /<T: BinaryInteger> (left: SCNVector3, right: T) -> SCNVector3 {
+        return SCNVector3(left.x / Float(right), left.y / Float(right), left.z / Float(right))
+    }
+    
+    static func /=<T: BinaryFloatingPoint> (left: inout SCNVector3, right: T) {
+        left = left / right
+    }
+    
+    static func /=<T: BinaryInteger> (left: inout SCNVector3, right: T) {
         left = left / right
     }
 }
